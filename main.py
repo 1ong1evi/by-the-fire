e773a0f2408fa5ce99dab42d8788440fa8336ca9
Levi <longlevi6@gmail.com>
2022-03-17 09:34:55 -0500

    more plot added to the story

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 
 
 def main():
@@ -22,7 +23,17 @@ Then all of sudden halfway through cooking it, you hear a noise in the woods.
 
     investigate = input("Do you go checkout the noise? (y/n) ")
     if investigate == "n":
-        return "You sleep through the cold night, to live another day.\nSafe travels, traveler."
+        print("\nYou sleep through the cold night, to live another day.\nSafe travels, traveler.")
+    sneak = input("\nYou are approaching the sound. Do you rush in? (y/n) ")
+    if sneak == "n":
+        strange_figure = input(
+            "You see a strange animal. Tall and standing on its hind legs.\n Do you continue to sneak to get a better look? (y/n)")
+    print("""\nYou rush in to only be met by a deformed animal with orange-red eyes.
+You turn your back to it while running back to your camp.
+That's when you feel sharp claws puncturing into both your shoulders.
+You try to stay upright but you fall to your knees. Then to the floor of
+the forest. You try to scream but it was only a thought. 
+""")
 
 
 main()
