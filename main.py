35385380722817622b5ca0985367c1bc3f69702d
Levi <longlevi6@gmail.com>
2022-03-20 08:22:26 -0500

    logic for to get a better look.. no answer

--- main.py
+++ main.py
@@ -21,13 +21,14 @@ Then all of sudden halfway through cooking it, you hear a noise in the woods.
 # Intro to the story & first question
 investigate = input("Do you go checkout the noise? (y/n) ")
 if investigate == "n":
-    print(
-        "\nYou sleep through the cold night, to live another day.\nSafe travels, traveler.")
+    print("\nYou sleep through the cold night, to live another day.\nSafe travels, traveler.")
 sneak = input("\nYou are approaching the sound. Do you rush in? (y/n) ")
 if sneak == "n":
     strange_figure = input(
         "\nYou see a strange animal. Tall and standing on its hind legs.\n Do you continue to sneak to get a better look? (y/n) ")
-    if strange_figure == "y":
+    if strange_figure == "n":
+        print("\nWeary of your travels you decide to head back to camp.\nYou sleep through the cold night, to live another day.\nSafe travels, traveler.")
+    elif strange_figure == "y":
         sneak_attack = input("""
 You see a deformed animal with orange-red eyes. Remembering of past conversations of the previous
 the village you know this a skinwalker. Due to you sneaking you have a chance to kill it. 
@@ -35,7 +36,7 @@ You have a 50% chance do you take it? (y/n) """)
         if sneak_attack == "n":
             print("\nUneasy about your chances you decide to sneak back to camp.\nYou sleep through the cold night, to live another day.\nSafe travels, traveler.")
         elif sneak_attack == "y":
-            one = "\nThe closer you get the more you second guess yourself.\nYou get one step away when you step on a leaf. The skinwalker turns around\nand gives out a loud screech you try to swing at it with your sword but you miss. It slashes at your throat.\nYou try to stay upright but you fall to your knees. Then to the floor of \nthe forest. You try to scream but it was only a thought"
+            one = "\nThe closer you get the more you second guess yourself.\nYou get one step away when you step on a leaf. The skinwalker turns around\nand gives out a loud screech, you try to swing at it with your sword but you miss. It slashes at your throat.\nYou try to stay upright but you fall to your knees. Then to the floor of \nthe forest. You try to scream but it was only a thought"
             two = "\nThe closer you get the more you second guess yourself.\nYou pull out your knife that was dipped in white ash. You smell the stench\nof death the skinwalker reeks of. In an instances you slit the skinwalker's throat. It drops to its knees without\na peep. You drag the it back to your camp."
             seq = [two, one]
             print(random.choice(seq))
